887ff31311b516738e53e2144203fcf4540e6a9c
tdtrung93 <tdtrung5893@gmail.com>
2018-08-18 10:36:54 +0700

    ScrollSpy version 1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-README.md
+# Example Scrollspy by tdt
--- src/scrollspy.js
+++ src/scrollspy.js
@@ -1,27 +1,28 @@
 export class ScrollSpy {
-  constructor (menu, options = {}){
-    if (!menu) throw new Error "Error"
-    if (typeof options != 'object') throw new Error "Error"
-    let default_option = {
+  constructor (menu, options = {}) {
+    let defaultOptions = {
       offset: 0,
       menu_target: 'li > a',
-      section: '.page',
-      menu_active: 'active',
+      section_class: '.scrollspy',
+      active_class: 'active',
     }
-    this.list_menu = menu instanceof HTMLElement ? menu : document.queryElement(menu)
-    this.options = Object.assign({}, default_option, options)
-    this.sections = document.queryElement(this.options.section)
+    this.list_menu = menu instanceof HTMLElement ? menu : document.querySelector(menu)
+    this.options = Object.assign({}, defaultOptions, options)
+    this.sections = document.querySelectorAll(this.options.section_class)
   }
-  onScroll (){
+  
+  onScroll () {
     this.sections.forEach((section) => {
-      let current_p = document.documentElement.scrollTop
-      let section_p = section.position().top
+      const current_p = document.documentElement.scrollTop
+      const section_p = section.offsetTop
       if (section_p <= current_p + this.options.offset) {
-        let section_id = document.getAttribute('id')
-        let active_menu = this.list_menu.querySelector(`[href="#${section_id}"]`)
-        let deactive_menu = this.list_menu.querySelector('[class="active"]')
+        const section_id = section.getAttribute('id')
+        const active_menu = this.list_menu.querySelector(`[href="#${section_id}"]`)
+        const deactive_menu = this.list_menu.querySelector(`[class="active"]`)
         deactive_menu.setAttribute('data-active', 'false')
+        deactive_menu.classList.remove(this.options.active_class)
         active_menu.setAttribute('data-active', 'true')
+        active_menu.classList.add(this.options.active_class)
       }
     })
   }
